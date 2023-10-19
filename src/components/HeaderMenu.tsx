c74e60d0e8ef3f99e61f2336ce85d5ab30d542ae
Varun Vasudeva <varunvasudeva1@gmail.com>
2023-10-18 21:16:52 -0400

    Add animations to header sidebar

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -5,7 +5,9 @@ import {
   RiCodeSSlashFill,
   RiVoiceprintFill,
   RiQuillPenLine,
+  RiMenu5Fill,
 } from "react-icons/ri";
+import { Transition } from "@headlessui/react";
 
 export const HeaderMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,40 +16,6 @@ export const HeaderMenu = () => {
     setIsOpen(!isOpen);
   };
 
-  return (
-    <>
-      <button
-        onClick={toggleSidebar}
-        className="sm:hidden flex items-center text-white text-xl lg:text-2xl p-1 rounded-full"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="#fff"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h16m-7 6h7"
-          ></path>
-        </svg>
-      </button>
-
-      {isOpen && (
-        <div
-          className="fixed top-0 left-0 w-screen h-screen bg-zinc-900 opacity-50 z-40"
-          onClick={toggleSidebar}
-        ></div>
-      )}
-      {isOpen && <HeaderSidebar closeSidebar={toggleSidebar} />}
-    </>
-  );
-};
-
-const HeaderSidebar = ({ closeSidebar }: { closeSidebar: () => void }) => {
   const items = [
     {
       link: "/",
@@ -72,31 +40,66 @@ const HeaderSidebar = ({ closeSidebar }: { closeSidebar: () => void }) => {
   ];
 
   return (
-    <div className="fixed top-0 right-0 h-screen w-56 bg-slate-950 z-50 shadow-lg overflow-y-auto">
+    <>
       <button
-        onClick={closeSidebar}
-        className="flex justify-end p-2 text-2xl text-white"
+        onClick={toggleSidebar}
+        className="sm:hidden flex items-center text-white text-xl lg:text-2xl p-1"
       >
-        <BsX />
+        <RiMenu5Fill />
       </button>
-      <div className="flex flex-col space-y-4 p-4">
-        {items.map((item, index) => (
-          <a
-            key={index}
-            href={item.link}
-            onClick={closeSidebar}
-            className="flex flex-row items-center justify-between text-lg lg:text-xl font-semibold text-gray-700 dark:text-gray-300 hover:opacity-40 transition-opacity ease-in-out duration-500"
+
+      {isOpen && (
+        <div
+          className="fixed top-0 left-0 w-screen h-screen bg-slate-900 opacity-50 z-40"
+          onClick={toggleSidebar}
+        ></div>
+      )}
+
+      <Transition
+        className="fixed top-0 right-0 h-screen w-56 lg:w-64 bg-slate-950 z-50 shadow-lg overflow-y-auto"
+        show={isOpen}
+        enter="transition ease-in-out duration-300 transform"
+        enterFrom="translate-x-full"
+        enterTo="translate-x-0"
+        leave="transition ease-in-out duration-300 transform"
+        leaveFrom="translate-x-0"
+        leaveTo="translate-x-full"
+      >
+        <nav
+          id="default-sidebar"
+          className={`fixed flex flex-col justify-start items-start top-0 right-0 w-56 p-4 h-screen bg-white dark:bg-slate-950 z-50 shadow-lg overflow-y-auto ${
+            isOpen
+              ? "transform translate-x-0 ease-in"
+              : "transform -translate-x-full ease-out"
+          }`}
+          aria-label="Sidebar"
+        >
+          <button
+            onClick={toggleSidebar}
+            className="flex self-end text-white text-2xl lg:text-3xl p-1"
           >
-            <span className="flex flex-row items-center space-x-2">
-              {item.icon === "RiHome2Line" && <RiHome2Line />}
-              {item.icon === "RiCodeSSlashFill" && <RiCodeSSlashFill />}
-              {item.icon === "RiVoiceprintFill" && <RiVoiceprintFill />}
-              {item.icon === "RiQuillPenLine" && <RiQuillPenLine />}
-              <span>{item.title}</span>
-            </span>
-          </a>
-        ))}
-      </div>
-    </div>
+            <BsX />
+          </button>
+          <div className="flex flex-col space-y-4 p-4">
+            {items.map((item, index) => (
+              <a
+                key={index}
+                href={item.link}
+                onClick={toggleSidebar}
+                className="flex flex-row items-center justify-between text-xl lg:text-2xl font-semibold text-gray-700 dark:text-gray-300 hover:opacity-40 transition-opacity ease-in-out duration-500"
+              >
+                <span className="flex flex-row items-center space-x-2">
+                  {item.icon === "RiHome2Line" && <RiHome2Line />}
+                  {item.icon === "RiCodeSSlashFill" && <RiCodeSSlashFill />}
+                  {item.icon === "RiVoiceprintFill" && <RiVoiceprintFill />}
+                  {item.icon === "RiQuillPenLine" && <RiQuillPenLine />}
+                  <span>{item.title}</span>
+                </span>
+              </a>
+            ))}
+          </div>
+        </nav>
+      </Transition>
+    </>
   );
 };
