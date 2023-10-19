d987f2482b6205c53cb8ba4ae6b859c2a979f630
Varun Vasudeva <varunvasudeva1@gmail.com>
2023-10-19 00:37:00 -0400

    Add blur to sidebar

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
-import { BsX } from "react-icons/bs";
+import { useState } from "react";
+import { BsX } from "react-icons/bs/index";
 import {
   RiHome2Line,
   RiCodeSSlashFill,
   RiVoiceprintFill,
   RiQuillPenLine,
   RiMenu5Fill,
-} from "react-icons/ri";
+} from "react-icons/ri/index";
+import { FaGithub, FaLinkedin } from "react-icons/fa/index";
+import { IoMdMail } from "react-icons/io/index";
 import { Transition } from "@headlessui/react";
 
 export const HeaderMenu = () => {
@@ -43,20 +45,20 @@ export const HeaderMenu = () => {
     <>
       <button
         onClick={toggleSidebar}
-        className="sm:hidden flex items-center text-white text-xl lg:text-2xl p-1"
+        className="lg:hidden flex items-center text-white text-xl lg:text-2xl p-1"
       >
         <RiMenu5Fill />
       </button>
 
       {isOpen && (
         <div
-          className="fixed top-0 left-0 w-screen h-screen bg-slate-900 opacity-50 z-40"
+          className="fixed top-0 left-0 w-screen h-screen bg-zinc-900 opacity-50 z-40"
           onClick={toggleSidebar}
         ></div>
       )}
 
       <Transition
-        className="fixed top-0 right-0 h-screen w-56 lg:w-64 bg-slate-950 z-50 shadow-lg overflow-y-auto"
+        className="fixed flex items-center top-0 right-0 h-screen w-56 lg:w-64 backdrop-blur-lg bg-zinc-800 bg-opacity-60 z-50 shadow-lg overflow-y-auto"
         show={isOpen}
         enter="transition ease-in-out duration-300 transform"
         enterFrom="translate-x-full"
@@ -67,7 +69,7 @@ export const HeaderMenu = () => {
       >
         <nav
           id="default-sidebar"
-          className={`fixed flex flex-col justify-start items-start top-0 right-0 w-56 p-4 h-screen bg-white dark:bg-slate-950 z-50 shadow-lg overflow-y-auto ${
+          className={`fixed flex flex-col justify-start items-start top-0 right-0 w-56 lg:w-64 p-4 h-screen overflow-y-auto ${
             isOpen
               ? "transform translate-x-0 ease-in"
               : "transform -translate-x-full ease-out"
@@ -97,6 +99,23 @@ export const HeaderMenu = () => {
                 </span>
               </a>
             ))}
+          </div>
+          <div class="flex flex-row items-center justify-end mx-4 p-3 space-x-2 bg-zinc-600 rounded-full">
+            <a
+              href="https://github.com/varunvasudeva1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub className="text-white hover:text-opacity-70 text-2xl lg:text-3xl mx-1 lg:mx-2" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/varunvasudeva1/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin className="text-white hover:text-opacity-70 text-2xl lg:text-3xl mx-1 lg:mx-2" />
+            </a>
+            <IoMdMail className="text-white hover:text-opacity-70 text-2xl lg:text-3xl mx-1 lg:mx-2" />
           </div>
         </nav>
       </Transition>
