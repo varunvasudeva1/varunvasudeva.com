5dfc2affe91e9dbf6311bf7aa0481a67ba5a694a
Varun Vasudeva <varunvasudeva1@gmail.com>
2024-04-18 14:37:08 -0500

    Fix Marquee component

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -4,15 +4,34 @@ export default function Marquee({
   items: { source: string; alt: string; style?: string }[];
 }) {
   return (
-    <div className="relative flex w-full overflow-x-hidden mt-8 mb-4 border-b-2 border-t-2 border-black font-bold">
-      <div className="flex flex-row animate-marquee whitespace-nowrap py-12 space-x-10">
+    <div className="relative flex w-full overflow-x-hidden mt-8 mb-4 border-b-2 border-t-2 border-black">
+      <div className="flex flex-row animate-marquee whitespace-nowrap py-12">
         {items.map((item) => {
           return (
             <img
               key={item.source}
               src={item.source}
               alt={item.alt}
-              className={["size-20 lg:size-24", item.style ?? ""].join(" ")}
+              className={[
+                "size-14 sm:size-18 lg:size-24 m-1",
+                item.style ?? "",
+              ].join(" ")}
+            />
+          );
+        })}
+      </div>
+
+      <div className="absolute top-0 flex flex-row animate-marquee2 whitespace-nowrap py-12">
+        {items.map((item) => {
+          return (
+            <img
+              key={item.source}
+              src={item.source}
+              alt={item.alt}
+              className={[
+                "size-14 sm:size-18 lg:size-24 m-1",
+                item.style ?? "",
+              ].join(" ")}
             />
           );
         })}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,8 +4,8 @@ export default {
   theme: {
     extend: {
       animation: {
-        marquee: "marquee 20s linear infinite",
-        marquee2: "marquee2 5s linear infinite",
+        marquee: "marquee 15s linear infinite",
+        marquee2: "marquee2 15s linear infinite",
       },
       keyframes: {
         marquee: {
